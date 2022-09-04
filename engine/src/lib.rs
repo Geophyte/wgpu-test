93760ff6a9290bf3d8d5332284cd2cce897b2287
Geophyte <47425764+Geophyte@users.noreply.github.com>
2022-09-04 19:44:15 +0200

    clear screen

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,3 +1,6 @@
+mod renderer;
+
+use renderer::Renderer;
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
@@ -12,13 +15,33 @@ pub async fn run() {
         .build(&event_loop)
         .expect("Failed to create window");
 
+    let mut renderer = Renderer::new(&window).await;
+
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Poll;
         match event {
-            Event::WindowEvent { window_id, event } if window_id == window.id() => match event {
-                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                _ => {}
-            },
+            Event::WindowEvent { window_id, event } if window_id == window.id() => {
+                if !renderer.input(&event) {
+                    match event {
+                        WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                        WindowEvent::Resized(physical_size) => renderer.resize(physical_size),
+                        WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                            renderer.resize(*new_inner_size)
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            Event::RedrawRequested(window_id) if window_id == window.id() => {
+                renderer.update();
+                match renderer.render() {
+                    Ok(_) => {}
+                    Err(wgpu::SurfaceError::Lost) => renderer.resize(renderer.size),
+                    Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                    Err(e) => eprintln!("{:?}", e),
+                }
+            }
+            Event::MainEventsCleared => window.request_redraw(),
             _ => {}
         }
     });
---
+++ engine/src/renderer.rs
@@ -0,0 +1,122 @@
+use winit::{event::WindowEvent, window::Window};
+
+pub struct Renderer {
+    surface: wgpu::Surface,
+    config: wgpu::SurfaceConfiguration,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    pub size: winit::dpi::PhysicalSize<u32>
+    //render_pipeline: wgpu::RenderPipeline,
+}
+
+impl Renderer {
+    pub async fn new(window: &Window) -> Self {
+        let size = window.inner_size();
+
+        let instance = wgpu::Instance::new(wgpu::Backends::all());
+
+        let surface = unsafe { instance.create_surface(window) };
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await
+            .unwrap();
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    features: wgpu::Features::empty(),
+                    limits: wgpu::Limits::default(),
+                    label: None,
+                },
+                None,
+            )
+            .await
+            .expect("Failed to create device and/or queue");
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface.get_supported_formats(&adapter)[0],
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,
+        };
+        surface.configure(&device, &config);
+
+        //let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        //    label: Some("Render Pipeline Layout"),
+        //    bind_group_layouts: &[],
+        //    push_constant_ranges: &[]
+        //});
+        //let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        //    label: Some("Render Pipeline"),
+        //    layout: Some(&render_pipeline_layout),
+        //
+        //});
+
+        return Self {
+            surface,
+            config,
+            device,
+            queue,
+            size
+        };
+    }
+
+    pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    // True if event was fully processed
+    pub fn input(&mut self, _: &WindowEvent) -> bool {
+        return false;
+    }
+
+    pub fn update(&mut self) {}
+
+    pub fn render(&self) -> Result<(), wgpu::SurfaceError> {
+        let output = self.surface.get_current_texture()?;
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+
+        {
+            let mut _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: None,
+            });
+        }
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
+}
